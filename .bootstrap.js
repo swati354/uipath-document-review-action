95687fb04ad126d507edbcec91850bfd2c8332a7
Nucleus <nucleus-bot@uipath.com>
2025-11-30 20:02:01 +0530

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun install hono"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
